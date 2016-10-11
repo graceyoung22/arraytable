32cc69159f756daed1eaa0d9a85d906d4548fb6b
Grace A Young <gay5@afsconnect1.njit.edu>
2016-10-10 22:04:12 -0400

    table

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+function build_table($array){
+// begins table
+$html = '<table>';
+// header 
+$html .= '<tr>';
+
+foreach($array[0] as $key=>$value){
+	$html .= '<th>' . $key . '</th>';
+
+}
+$html .= '</tr>';
+
+// data
+foreach ($array as $key=>$value){
+	$html .= '<tr>';
+	foreach($value as $key2=>$value2){
+		$html .= '<td>' . $value2 . '</td>';
+	}
+	$html .= '</tr>';
+}
+
+// return table
+$html .= '</table>';
+return $html;
+}
+
+$array = array( 
+	array('key'=>'word1', 'value'=>1),
+	array('key'=>'word2', 'value'=>2),
+	array('key'=>'word3', 'value'=>3),
+);
+
+echo build_table($array);
+
+?>		
